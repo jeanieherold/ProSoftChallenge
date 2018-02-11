30ce5d8acd0f200a44911d8b569cc2b34aa14fd3
jeanieherold <jeaniemherold68@gmail.com>
2018-02-11 13:07:10 -0500

    starting calendar object

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,24 @@ Create a calendar from user input according to the guidelines provided in docume
 I wont finish this in 2.5 hours but I want to create the calendar - just to do it. 
 */
 
+//Create a calendar object when submit is clicked
 var calendarTemplate = "<p>helloworld</p>";
 
 $('.btn-submit').click(function() {
+	var make = new Calendar('test', 10, 'US');
+	make.showCalendar();
 	$('#results').html(calendarTemplate);
 	return false;
 });
+
+//Calendar object
+
+var Calendar = function(startdate, numdays, countrycode) {
+	this.startdate = startdate;
+	this.numdays = numdays;
+	this.countrycode = countrycode;
+}
+
+Calendar.prototype.showCalendar = function() {
+	console.log('show calendar');
+};
