511cbaa9df4edb456b84ff541a47e4967b542098
jeanieherold <jeaniemherold68@gmail.com>
2018-02-11 11:23:53 -0500

    base file setup

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+/* 
+Prosoft Calendar Challenge
+author: Jeanie Herold
+date: 2/11/2018
+Challenge:
+Create a calendar from user input according to the guidelines provided in document.
+I wont finish this in 2.5 hours but I want to create the calendar - just to do it. 
+*/
+
