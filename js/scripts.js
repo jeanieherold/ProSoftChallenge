8afa6ba78584ded77fae229c847c73ce4878aa0c
jeanieherold <jeaniemherold68@gmail.com>
2018-02-11 14:01:25 -0500

    calendar object with prototype to display calendar template

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,12 @@ I wont finish this in 2.5 hours but I want to create the calendar - just to do i
 $('.btn-submit').click(function() {
 	$('.calTemplate').show();
 	var make = new Calendar('test', 10, 'US');
-	make.showCalendar();
-	$('#results').html(calendarTemplate);
+	//display calendar on webpage
+	$('#results').html(make.showCalendar());
 	return false;
 });
 
 //Calendar object
-
 var Calendar = function(startdate, numdays, countrycode) {
 	this.startdate = startdate;
 	this.numdays = numdays;
@@ -26,13 +25,60 @@ var Calendar = function(startdate, numdays, countrycode) {
 
 Calendar.prototype.showCalendar = function() {
 	console.log('show calendar');
-	calendarTemplate(10);
-};
-
-function calendarTemplate(numdays) {
-	var template = "";
-	var calendarHTML = "";
+	var calendarHTML = '';
+	calendarHTML = `
+					<table class="calTemplate">
+    				<thead>
+    					<th class="day-char">S</th>
+    					<th class="day-char">M</th>
+    					<th class="day-char">T</th>
+    					<th class="day-char">W</th>
+    					<th class="day-char">T</th>
+    					<th class="day-char">F</th>
+    					<th class="day-char">S</th>
+    				</thead>
+            <tr class="month">
+                <td class="month-name" colspan="7">January 2018</td>
+            </tr>
+    				<tr>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    				</tr>
+    				<tr>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    				</tr>
+    				<tr>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    				</tr>
+    				<tr>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    					<td class="day-cell"></td>
+    				</tr>
+    			</table>
+	`;
 
 	return calendarHTML;
 
-}
+};
